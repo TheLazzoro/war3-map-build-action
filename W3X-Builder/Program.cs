513156ee30af00f0aa535eaae3736dc1814cb7e3
TheLazzoro <lasse.d@live.dk>
2024-12-29 14:10:00 +0100

    WIP build map

--- W3X-Builder/Program.cs
+++ W3X-Builder/Program.cs
@@ -1,9 +1,32 @@
-﻿using CommandLine;
+﻿using War3Net.Build;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Testing custom GH action");
+var workingDir = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
+Console.WriteLine($"Working directory: {workingDir}");
+var files = Directory.GetFileSystemEntries(workingDir, ".w3x, .w3m", SearchOption.AllDirectories);
 
-var projectDir = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
-Console.WriteLine($"Project directory: {projectDir}");
-Directory.GetFileSystemEntries(projectDir, "*", SearchOption.AllDirectories).ToList().ForEach(x => Console.WriteLine(x));
+if (files.Length == 0)
+{
+    Console.WriteLine($"Did not find any Warcraft III map files");
+    throw new Exception();
+}
+else if (files.Length > 1)
+{
+    Console.WriteLine($"Multiple Warcraft III map files found in repository. This action can only continue with a single map file.");
+    Console.WriteLine($"Found:");
+    foreach (var file in files)
+    {
+        Console.WriteLine(file);
+    }
+    throw new Exception();
+}
+
+string mapFile = files[0];
+string name = Path.GetFileName(mapFile);
+string fullPath = Path.Combine(workingDir, name);
+
+Console.WriteLine("Building map...");
+var map = Map.Open(mapFile);
+var builder = new MapBuilder(map);
+builder.Build(fullPath);
+Console.WriteLine("Build complete!");
+Console.WriteLine("Path: " + fullPath);
