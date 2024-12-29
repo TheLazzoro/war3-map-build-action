0ddd324b94a448c3590558360a8b24bfc6081732
TheLazzoro <lasse.d@live.dk>
2024-12-29 02:09:27 +0100

    testing working dir

--- W3X-Builder/Program.cs
+++ W3X-Builder/Program.cs
@@ -3,3 +3,7 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Testing custom GH action");
+
+var projectDir = Environment.GetEnvironmentVariable("GITHUB_WORKSPACE");
+Console.WriteLine($"Project directory: {projectDir}");
+Directory.GetFileSystemEntries(projectDir, "*", SearchOption.AllDirectories).ToList().ForEach(x => Console.WriteLine(x));
